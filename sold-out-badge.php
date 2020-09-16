b18adb4b52955b0eca98a48b5fe4fcfd1cf7643e
Luca Scalzotto <luca@scorpiac.com>
2020-09-16 22:34:30 +0200

    Namespace plugin, start on localization

--- sold-out-badge.php
+++ sold-out-badge.php
@@ -7,19 +7,31 @@
  * Requires PHP:      7.0
  * Author:            Luca Scalzotto
  * Author URI:        https://github.com/LucaScorpion
+ * Text Domain:       woocommerce-sold-out-badge
+ * Domain Path:       /languages
  */
+
+namespace WooCommerceSoldOutBadge;
+
+function init_hooks() {
+    add_action('plugins_loaded', __NAMESPACE__ . '\load_text_domain');
+    add_action('woocommerce_before_shop_loop_item_title', __NAMESPACE__ . '\show_sold_out_badge');
+    add_action('wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_badge_style');
+}
+
+function load_text_domain() {
+    load_plugin_textdomain('woocommerce-sold-out-badge', false, dirname(plugin_basename(__FILE__)) . '/languages');
+}
 
 function show_sold_out_badge() {
     global $product;
     if (!$product->is_in_stock()) {
-        printf('<span class="sold-out-badge">Sold Out</span>');
+        printf('<span class="sold-out-badge">%s</span>', __('Sold Out', 'woocommerce-sold-out-badge'));
     }
 }
-
-add_action('woocommerce_before_shop_loop_item_title', 'show_sold_out_badge');
 
 function enqueue_badge_style() {
     wp_enqueue_style('woocommerce-sold-out-badge', plugins_url('/sold-out-badge.css', __FILE__));
 }
 
-add_action('wp_enqueue_scripts', 'enqueue_badge_style');
+init_hooks();

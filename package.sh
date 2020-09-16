36fd99a60a7260f66ffe711458b0d141d0763a29
Luca Scalzotto <luca@scorpiac.com>
2020-09-16 22:54:19 +0200

    Set up Dutch localization

--- package.sh
+++ package.sh
@@ -3,9 +3,11 @@ set -eu
 
 # Clean up any previous build artifacts.
 [ -f woocommerce-sold-out-badge.zip ] && rm woocommerce-sold-out-badge.zip
+[ -d woocommerce-sold-out-badge ] && rm -r woocommerce-sold-out-badge
 
 # Copy the files into a directory and zip it.
 mkdir woocommerce-sold-out-badge
 cp ./*.{php,css,md} woocommerce-sold-out-badge
+cp -r languages woocommerce-sold-out-badge
 zip -r woocommerce-sold-out-badge.zip woocommerce-sold-out-badge
 rm -r woocommerce-sold-out-badge
